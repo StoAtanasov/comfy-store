c7b9a0a425c7d0baf0dae2cedf9cd26c4a63ae94
StoAtanasov <stoatanasov@yahoo.com>
2024-01-16 15:10:04 +0000

    Login and Register components added to the route structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import { About, Error, HomeLayout } from "./pages";
+import { Error, HomeLayout, Login, Register } from "./pages";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
     errorElement: <Error />,
   },
 ]);
