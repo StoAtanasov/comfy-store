8711acc0a095500e3ae55019d859ba7cc1c0bbb9
StoAtanasov <stoatanasov@yahoo.com>
2024-01-16 15:27:31 +0000

    Temporary navbar created

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,6 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 const HomeLayout = () => {
   return (
-    <div>HomeLayout</div>
-  )
-}
-export default HomeLayout
+    <>
+      <nav>
+        <span className="text-4xl text-primary">Comfy</span>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+export default HomeLayout;
