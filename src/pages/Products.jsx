45134c629a89588eae058e020fbc996a603e1a26
StoAtanasov <stoatanasov@yahoo.com>
2024-02-01 05:29:52 +0000

    Functionality of loader in Products page built.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,12 @@
 import { Filters, PaginationContainer, ProductsContainer } from "../components";
+import { customFetch } from "../utils";
 
+const url = "/products";
 export const loader = async ({ request }) => {
-  return null;
+  const  response  = await customFetch(url);
+  const products = response.data.data;
+  const meta = response.data.meta;
+  return {products, meta};
 };
 
 const Products = () => {
